c27708683b7f73be9bbe662f96bfb5795c9957fd
ryatran <ryatran@pdx.edu>
2026-05-18 13:55:49 -0700

    adding argparse for output

--- aleatoric.py
+++ aleatoric.py
@@ -1,7 +1,11 @@
 from scipy.io.wavfile import write
 import numpy as np
 import sounddevice as sd
+import argparse
 
+aleatoric_parser = argparse.ArgumentParser(description="Aleatoric Music Generator")
+aleatoric_parser.add_argument('--output', '-o', type=str, help="Output to WAV instead of Playing Directly")
+args = aleatoric_parser.parse_args()
 
 def generate_song() -> np.ndarray:
     """With specific parameters.
@@ -12,3 +16,7 @@ def generate_song() -> np.ndarray:
 
 if __name__ == "__main__":
     generated_song = generate_song()
+    if args.output:
+        print(f"When done, this song will be turned into a WAV file called {args.output}")
+    else:
+        print("When done, this song will be played directly.")
