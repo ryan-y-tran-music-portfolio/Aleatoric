04373ce8d5e21886d85880bc3dc0bb0462d2e2d3
ryatran <ryatran@pdx.edu>
2026-05-18 13:46:26 -0700

    intital commit for generate_song

--- aleatoric.py
+++ aleatoric.py
@@ -1,3 +1,14 @@
 from scipy.io.wavfile import write
 import numpy as np
 import sounddevice as sd
+
+
+def generate_song() -> np.ndarray:
+    """With specific parameters.
+
+    Returns: np.ndarray of generated song.
+    """
+
+
+if __name__ == "__main__":
+    generated_song = generate_song()
